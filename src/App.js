a80a56e935ecf85adb289e4b790ad2e411aa5937
Irehmanullah <irehmanullah@gmail.com>
2021-06-16 17:13:32 +0500

    improved

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  console.log(todos)
   return (
     <div className="App">
       <header>
