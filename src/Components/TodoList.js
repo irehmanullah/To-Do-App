be70fd39224f5be63b34bccf0c0494307f2647eb
irehmanullah <irehmanullah@gmail.com>
2021-06-21 23:31:37 +0500

    improvements

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,7 +4,10 @@ const TodoList = ({ filteredTodos, todos, setTodos, setInputTodo, setInputText }
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                {filteredTodos.map((todo) => (<Todo text={todo.text} message={todo.Todo} setInputText={setInputText} setInputTodo={setInputTodo} key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />))}
+                {filteredTodos.map((todo) => (<Todo text={todo.text}
+                    message={todo.Todo} setInputText={setInputText}
+                    setInputTodo={setInputTodo} key={todo.id}
+                    todo={todo} todos={todos} setTodos={setTodos} />))}
             </ul>
         </div>
     )
